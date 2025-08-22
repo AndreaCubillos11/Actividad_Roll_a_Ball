e55cc4136a8f7f9e82ca086d702132c0849af5d1
Andrea Cubillos <Andreav.cubillosp@konradlorenz.edu.co>
2025-08-22 12:20:59 -0500

    Movimiento básico, cámara y elementos iniciales
    Se implementó el control de la esfera con Input.GetAxis, seguimiento de cámara con offset y se agregaron paredes y un objeto recolectable.

---
+++ Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() //Se invoca cuando inicia el videojuego, como si fuera una clase de constructor
+    {
+        Debug.Log("Hola Mundo...");
+    }
+
+    // Update is called once per frame
+    void Update() //se ejecuta por cada frame (actualización de la pantalla según el diapositivo) del videojuego 
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    private Rigidbody rb;
+    public float speed;
+    void Start()
+    {
+        rb=GetComponent<Rigidbody>();// Obtener un componente del objeto asociado  
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate(){
+
+        // se ejcuta muchas veces por segundo
+        float moveHorizontal=Input.GetAxis("Horizontal");
+        float moveVertical=Input.GetAxis("Vertical");
+
+        Vector3 movimiento=new Vector3(moveHorizontal,0.0f,moveVertical);
+        
+        rb.AddForce(movimiento*speed);
+    }
+}
