779c9432c2a064d25965ae2cec27c03e6ab523ba
Andrea Cubillos <Andreav.cubillosp@konradlorenz.edu.co>
2025-08-26 20:10:42 -0500

    Implementación de rotación, detección de colisiones y sistema de partículas en objetos recolectables
    Se implementó la rotación de objetos utilizando `transform.Rotate()` con `Time.deltaTime` para mantener una velocidad constante sin depender de la tasa de fotogramas. Se añadió detección de colisiones mediante el método `OnTriggerEnter(Collider other)` y se validó si el objeto colisionado tiene la etiqueta "Recolectable" usando `CompareTag`. Al detectar una colisión con un objeto recolectable, este se oculta con `SetActive(false)`. Además, se integró un sistema de partículas, obteniendo el componente `ParticleSystem` desde un `Transform`, el cual se activa solo al recolectar cubos y se mantiene detenido inicialmente con `systemParticulas.Stop()`.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,17 @@ public class PlayerController : MonoBehaviour
     
     private Rigidbody rb;
     public float speed;
+    public Transform particulas;
+    private ParticleSystem systemParticulas;
+    private Vector3 posicion; 
+
     void Start()
     {
         rb=GetComponent<Rigidbody>();// Obtener un componente del objeto asociado  
+
+        systemParticulas = particulas.GetComponent<ParticleSystem> ();
+
+        systemParticulas.Stop();
         
     }
 
@@ -27,5 +35,15 @@ public class PlayerController : MonoBehaviour
         Vector3 movimiento=new Vector3(moveHorizontal,0.0f,moveVertical);
         
         rb.AddForce(movimiento*speed);
+    }
+
+    void OnTriggerEnter (Collider other){
+
+        if (other.gameObject.CompareTag ("Recolectable")){
+            //el objeto es recolectable
+            other.gameObject.SetActive (false);
+        } else {
+            //el objeto No es recolectable
+        }
     }
 }
--- Assets/Scripts/Rotador.cs
+++ Assets/Scripts/Rotador.cs
@@ -13,6 +13,6 @@ public class Rotador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate (new Vector3 (45,45,45) * Time.deltaTime);
     }
 }
