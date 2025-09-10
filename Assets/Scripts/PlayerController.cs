73ff3104b8d5b400013f5893e0cb2c43d4322566
Andrea Cubillos <Andreav.cubillosp@konradlorenz.edu.co>
2025-09-09 19:51:43 -0500

    Implementación de sistema de partículas, sonido de colisión y transición de escena
    Se añadió un sistema de partículas que se posiciona dinámicamente en el lugar de colisión utilizando la posición del objeto recolectable (other.gameObject.transform.position). El sistema de partículas se reutiliza y se activa con Play() en cada colisión.
    Además, se integró un efecto de sonido (Weapons of Choice) que se reproduce al momento de la colisión de la pelota con un objeto recolectable.
    También se implementó un contador que permite cambiar de escena automáticamente tras alcanzar cierta condición. Durante este proceso, los objetos del nivel anterior se descargan de memoria y se inicializan los elementos del nuevo nivel, mostrando una pantalla de carga mientras se produce la transición.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,40 +11,65 @@ public class PlayerController : MonoBehaviour
     public Transform particulas;
     private ParticleSystem systemParticulas;
     private Vector3 posicion; 
+    private AudioSource audioRecoleccion;
+    
+    // Esta es tu variable para el contador
+    private int contador = 0; 
+
+    // Aquí puedes definir cuántos cubos necesitas para ganar.
+    public int cubosParaGanar = 12;
 
     void Start()
     {
-        rb=GetComponent<Rigidbody>();// Obtener un componente del objeto asociado  
-
-        systemParticulas = particulas.GetComponent<ParticleSystem> ();
-
+        rb = GetComponent<Rigidbody>();
+        systemParticulas = particulas.GetComponent<ParticleSystem>();
         systemParticulas.Stop();
-        
-    }
 
-    // Update is called once per frame
+        audioRecoleccion = GetComponent<AudioSource> ();
+    }
+    
+    // El método Update y FixedUpdate están correctos
     void Update()
     {
-        
+        // No hay necesidad de código aquí para el contador
     }
+
     void FixedUpdate(){
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-        // se ejcuta muchas veces por segundo
-        float moveHorizontal=Input.GetAxis("Horizontal");
-        float moveVertical=Input.GetAxis("Vertical");
-
-        Vector3 movimiento=new Vector3(moveHorizontal,0.0f,moveVertical);
+        Vector3 movimiento = new Vector3(moveHorizontal, 0.0f, moveVertical);
         
-        rb.AddForce(movimiento*speed);
+        rb.AddForce(movimiento * speed);
     }
 
     void OnTriggerEnter (Collider other){
 
-        if (other.gameObject.CompareTag ("Recolectable")){
-            //el objeto es recolectable
-            other.gameObject.SetActive (false);
-        } else {
-            //el objeto No es recolectable
+        audioRecoleccion.Play();
+
+        // Aquí se comprueba si el objeto con el que colisionas tiene la etiqueta correcta
+        if (other.gameObject.CompareTag("Recolectable"))
+        {
+            // Ahora, el contador solo se incrementa si el objeto es recolectable
+            contador = contador + 1;
+
+            posicion = other.gameObject.transform.position;
+            particulas.position = posicion;
+
+            
+            systemParticulas.Play();
+
+            other.gameObject.SetActive(false);
+
+            // Se comprueba si la cantidad de cubos recolectados es igual o mayor a la meta
+            if (contador >= cubosParaGanar)
+            {
+                // Se ejecuta esta lógica solo si la condición se cumple
+                Debug.Log("¡Has recolectado todos los cubos!");
+
+                // Se carga la siguiente escena 
+                SceneManager.LoadScene(1);
+            }
         }
     }
 }
